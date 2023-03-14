37c45e2039d09364eb73671b686fbd938c84e26d
Yuheng Ding <yuheng@Yuhengs-MacBook-Pro.local>
2023-03-14 15:35:33 -0700

    fixed all bugs

--- src/hooks/usePaginatedTransactions.ts
+++ src/hooks/usePaginatedTransactions.ts
@@ -22,7 +22,7 @@ export function usePaginatedTransactions(): PaginatedTransactionsResult {
         return response
       }
 
-      return { data: response.data, nextPage: response.nextPage }
+      return { data: [...(paginatedTransactions === null ? [] : paginatedTransactions.data), ...response.data], nextPage: response.nextPage }
     })
   }, [fetchWithCache, paginatedTransactions])
 
